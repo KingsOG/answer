bfa100f195aec409988fdbfbc21e1475c922a869
Kingsley Ogbonna <kingsog@Kingsleys-MacBook-Air.local>
2022-04-12 06:56:30 +0100

    first commit

---
+++ jscript/main.js
@@ -0,0 +1,12 @@
+let total = 0
+
+document.getElementById('').onclick = workItOut
+document.getElementById('').onclick = equalItOut
+
+function workItOut(){
+    total +=
+    document.querySelector('#inputext').innerText = total
+}
+function equalItOut(){
+    document.querySelector('').innerText = total
+}
